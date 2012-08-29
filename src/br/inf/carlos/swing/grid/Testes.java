e4710a44fd4c59e590f461815c2457d653301d81
carlosjrcabello <carlos.spohr@gmail.com>
2012-08-28 22:41:38 -0300

    :D

--- src/br/inf/carlos/swing/grid/Testes.java
+++ src/br/inf/carlos/swing/grid/Testes.java
@@ -2,40 +2,52 @@ package br.inf.carlos.swing.grid;
 
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
-import java.util.ArrayList;
-import java.util.Collection;
 
-import javax.swing.JButton;
-import javax.swing.JComponent;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class Testes {
 
 	public static void main(String[] args) {
-		GridBag grid = new Grid();
+		Gridify gridify = new GridBagConstraintsImpl();
 		
 		JPanel panel =  new JPanel(new GridBagLayout());
-		JButton bu = new JButton("champz");
 		
-		Collection<JComponent> buttons = new ArrayList<JComponent>(0);
+		// Login icon.
+		Icon icon = new ImageIcon("./preferences_system_login.png");
+		gridify.atLine(0).atColumn(0).rowspan().add(new JLabel(icon), panel);
 		
-		for (int i = 0; i < 10; i++) {
-			JButton b = new JButton("bt: " + i);
-			buttons.add(b);
-		}
+		// Username line.
+		gridify.atLine(1).atColumn(1).margins(5).add(new JLabel("Username:"), panel);
+//		JTextField username = new JTextField();
+//		username.setPreferredSize(new Dimension(150, 21));
+//		gridify.atLine(1).atColumn(2).add(username, panel);
+//		
+//		
+//		// Password line.
+		gridify.atLine(2).atColumn(1).margins(5).add(new JLabel("Password:"), panel);
+//		JPasswordField password = new JPasswordField();
+//		gridify.atLine(2).atColumn(2).fill().top(5).add(password, panel);
 		
-		grid.atLine(2).atColumn(0).withMargins(10, 10, 10, 10).alignment(GridBag.LEFT).add(buttons, panel);
+		// Kill session. (this uses two lines)
+		JCheckBox killSession = new JCheckBox("OMG, don't kill my session through ignorance");
 		
-		grid.atLine(0).atColumn(0).withMargins(10, 10, 10, 10).add(new JButton("Fuu..."), panel);
-		grid.atLine(0).atColumn(1).colspan(buttons.size() -1).withMargins(10, 10, 10, 10).add(bu, panel);
+		gridify.atLine(3).atColumn(0).colspan(3).top(5).add(killSession, panel);
+		
 		
 		JFrame frame = new JFrame();
 
 		frame.getContentPane().add(panel);
-		frame.setSize(new Dimension(900, 300));
+		frame.setSize(new Dimension(600, 300));//
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setTitle("Teste");
+		frame.pack();
 	}
 }
