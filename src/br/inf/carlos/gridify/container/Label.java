b5aeddcb36689f6db633e15cdfb8c6b94db8823a
carlos.spohr@gmail.com <carlos.spohr@gmail.com>
2013-03-18 09:15:30 -0300

    bugfix with labels

---
+++ src/br/inf/carlos/gridify/container/Label.java
@@ -0,0 +1,13 @@
+package br.inf.carlos.gridify.container;
+
+import javax.swing.JLabel;
+
+public class Label extends JLabel{
+
+	private static final long serialVersionUID = 1L;
+
+	public Label(String text) {
+		super(text);
+	}
+
+}
